8568170fb616ac341f7d24d1805051a68b53fe6f
LIU-HONGYANG <1664698982@qq.com>
2020-11-28 13:39:47 +0800

    Create BadSuspend.java

---
+++ code/java/javaconcurrency/chapter2/BadSuspend.java
@@ -0,0 +1,31 @@
+public class BadSuspend {
+
+    public static Object u = new Object();
+
+    static ChangeObjectThread t1 = new ChangeObjectThread("t1");
+    static ChangeObjectThread t2 = new ChangeObjectThread("t2");
+
+    public static class ChangeObjectThread extends Thread{
+        public ChangeObjectThread(String name){
+            super.setName(name);
+        }
+
+        public void run(){
+            synchronized (u){
+                System.out.println("in" + getName());
+                Thread.currentThread().suspend();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        t1.start();
+        t2.start();
+        t1.resume();
+        t2.resume();
+        t1.join();
+        t2.join();
+    }
+
+
+}
